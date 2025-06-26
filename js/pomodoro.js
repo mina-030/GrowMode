0f464102b5cb5868f764e6f6781c4edcd6fe631e
mina-030 <qqmina995@gmail.com>
2025-06-26 15:24:48 -0400

    adding rest mode

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -2,16 +2,25 @@ const start = document.getElementById("start");
 const stop = document.getElementById("stop");
 const reset = document.getElementById("reset");
 const timer = document.getElementById("timer");
+const rest = document.getElementById("rest");
 
+let isRestMode = false;
 let interval;
 let timeLeft = 1500; /* base on second, so if 25min = 1500s */
 
+// Update the timer display
 function updateTimer(){
     let minutes = Math.floor(timeLeft/60);
     let seconds = timeLeft % 60;
     let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 
     timer.innerHTML = formattedTime;
+}
+
+//Set time based on mode
+function correctTime() {
+    timeLeft = isRestMode? 300 : 1500;
+    updateTimer();
 }
 
 // The Start Timer Function
@@ -24,10 +33,9 @@ function startTimer(){
             clearInterval(interval);
             interval = null; //clear the interval reference
             alert("Time's up!");
-            timeLeft = 1500;
-            updateTimer();
+            correctTime();
         }
-    }, 1000)
+    }, 1000);
 }
 
 // The Stop Timer Function
@@ -40,11 +48,38 @@ function stopTimer(){
 function resetTimer(){
     clearInterval(interval);
     interval = null;
-    timeLeft = 1500;
-    updateTimer();
+    correctTime();
+}
+
+let title = document.querySelector(".title");
+let switchButtom = document.getElementById("rest");
+
+// The Rest Mode On Function
+function restMode() {
+    isRestMode = true;
+    correctTime();
+
+    title.innerText = "Take a breath";
+    switchButtom.innerText = "Work";
+
+    switchButtom.removeEventListener("click", restMode);
+    switchButtom.addEventListener("click", studyMode);
+}
+
+// The Study Mode On Function
+function studyMode() {
+    isRestMode = false;
+    correctTime();
+
+    title.innerText = "Working";
+    switchButtom.innerText = "Rest";
+
+    switchButtom.removeEventListener("click", studyMode);
+    switchButtom.addEventListener("click", restMode);
 }
 
 // Will active when the button is clicked
 start.addEventListener("click", startTimer);
 stop.addEventListener("click", stopTimer);
 reset.addEventListener("click", resetTimer);
+rest.addEventListener("click", restMode);
